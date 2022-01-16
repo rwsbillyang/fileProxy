d636ffe1b132723e53faa60051ad31e5204f48d2
BillYang <rwsbillyang@qq.com>
2022-01-16 14:44:13 +0800

    use CIO engine instead of Apache

--- src/Application.kt
+++ src/Application.kt
@@ -2,62 +2,99 @@ package com.github.rwsbillyang.fileProxy
 
 import io.ktor.application.*
 import io.ktor.client.*
-import io.ktor.client.engine.apache.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.features.*
 import io.ktor.client.statement.*
 import io.ktor.features.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import java.io.IOException
+//
+//val client = HttpClient(Apache) {
+//    engine {
+//        /**
+//         * Apache embedded http redirect, default = false. Obsolete by `HttpRedirect` feature.
+//         * It uses the default number of redirects defined by Apache's HttpClient that is 50.
+//         */
+//        followRedirects = true
+//
+//        /**
+//         * Timeouts.
+//         * Use `0` to specify infinite.
+//         * Negative value mean to use the system's default value.
+//         */
+//
+//        /**
+//         * Max time between TCP packets - default 10 seconds.
+//         */
+//        socketTimeout = 20_000
+//
+//        /**
+//         * Max time to establish an HTTP connection - default 10 seconds.
+//         */
+//        connectTimeout = 10_000
+//
+//        /**
+//         * Max time for the connection manager to start a request - 20 seconds.
+//         */
+//        connectionRequestTimeout = 20_000
+//
+//        customizeClient {
+//            // this: HttpAsyncClientBuilder
+//            // setProxy(HttpHost("127.0.0.1", 8080))
+//
+//            // Maximum number of socket connections.
+//            setMaxConnTotal(40960)
+//
+//            // Maximum number of requests for a specific endpoint route.
+//            //在请求同一个host:port时，会复用已经建立的连接,允许同一个host:port最多有几个活跃连接
+//            setMaxConnPerRoute(10240)
+//
+//            // ...
+//        }
+//        customizeRequest {
+//            // this: RequestConfig.Builder from Apache.
+//        }
+//    }
+//}
 
-val client = HttpClient(Apache) {
+val client = HttpClient(CIO) {
+    install(HttpTimeout) {}
+
+
+    //https://ktor.io/docs/http-client-engines.html#jvm-and-android
     engine {
-        /**
-         * Apache embedded http redirect, default = false. Obsolete by `HttpRedirect` feature.
-         * It uses the default number of redirects defined by Apache's HttpClient that is 50.
-         */
-        followRedirects = true
+        maxConnectionsCount = 20480
 
-        /**
-         * Timeouts.
-         * Use `0` to specify infinite.
-         * Negative value mean to use the system's default value.
-         */
+        endpoint {
+            /**
+             * Maximum number of requests for a specific endpoint route.
+             */
+            maxConnectionsPerRoute = 10240
 
-        /**
-         * Max time between TCP packets - default 10 seconds.
-         */
-        socketTimeout = 20_000
+            /**
+             * Max size of scheduled requests per connection(pipeline queue size).
+             */
+            pipelineMaxSize = 20
 
-        /**
-         * Max time to establish an HTTP connection - default 10 seconds.
-         */
-        connectTimeout = 10_000
+            /**
+             * Max number of milliseconds to keep idle connection alive.
+             */
+            keepAliveTime = 5000
 
-        /**
-         * Max time for the connection manager to start a request - 20 seconds.
-         */
-        connectionRequestTimeout = 20_000
+            /**
+             * Number of milliseconds to wait trying to connect to the server.
+             */
+            connectTimeout = 10000
 
-        customizeClient {
-            // this: HttpAsyncClientBuilder
-            // setProxy(HttpHost("127.0.0.1", 8080))
-
-            // Maximum number of socket connections.
-            setMaxConnTotal(40960)
-
-            // Maximum number of requests for a specific endpoint route.
-            //在请求同一个host:port时，会复用已经建立的连接,允许同一个host:port最多有几个活跃连接
-            setMaxConnPerRoute(10240)
-
-            // ...
-        }
-        customizeRequest {
-            // this: RequestConfig.Builder from Apache.
+            /**
+             * Maximum number of attempts for retrying a connection.
+             */
+            connectAttempts = 3
         }
     }
 }
-
 
 //https://github.com/ktorio/ktor-samples/blob/master/generic/samples/reverse-proxy/src/ReverseProxyApplication.kt
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
